e1ff110885ca709bea3ebc6b64b1ed4190b13f28
Greg Poole <greg.poole@domain.com.au>
2016-07-18 21:53:34 +1000

    Add failing import test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,19 @@
 
 var nodeSassImport = require('../');
 var test = require('tape');
+var sass = require('node-sass');
 
 test('base:function:test', function (t) {
   t.ok(typeof nodeSassImport === 'function');
   t.end();
+});
+
+test('base:importer:test', function(t) {
+  sass.render({
+    file: 'test/test.scss',
+    importer: nodeSassImport
+  }, function(err, result) {
+    t.error(err);
+    t.end();
+  });
 });
